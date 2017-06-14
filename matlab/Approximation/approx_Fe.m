137ba311d46b8f6a604afa83e2494638f8c15b1a
Jimmy Beaudoin <jbeaudoin11@gmail.com>
2017-06-13 23:05:28 -0400

    debug approx

--- matlab/Approximation/approx_Fe.m
+++ matlab/Approximation/approx_Fe.m
@@ -1,52 +1,93 @@
-clc, clear, close, figure, warning off;
+clc, clear, close all;
 
+distcomp.feature( 'LocalUseMpiexec', false ); % Pour run le code en parallel (parfor)
 
 %% Fe(z, i)
 load('Fe_attraction.mat')
 
-%% 1
-data_x = z_m1A;
-data_y = Fe_m1A;
-to_x = data_x(end);
-ik = -1;
 
-[f, gof] = fit(data_x, data_y, 'rat03', 'Upper', [-15 Inf Inf Inf], 'Lower', [-15 -Inf -Inf -Inf]);
+%% Const
+data_x_1 = z_m1A;
+data_y_1 = Fe_m1A;
+to_x_1 = data_x_1(end);
+ik_1 = -1;
 
-cv = coeffvalues( f );
-bE1_1 = cv(1)/ik - abs(ik);
-s_1 = @(x, ik) (ik*abs(ik) + bE1_1*ik)/(x^3 + cv(2)*x^2 + cv(3)*x + cv(4));
+data_x_2 = z_m2A;
+data_y_2 = Fe_m2A;
+to_x_2 = data_x_2(end);
+ik_2 = -2;
 
-rsquared(data_x, data_y, @(x) s_1(x, ik))
+eq_base = @(x, ik, cv, b_E1) (ik*abs(ik) + b_E1*ik)./(x.^3 + cv(2).*x.^2 + cv(3).*x + cv(4));
 
-subplot(2, 1, 1)
-scatter(data_x, data_y)
-hold on
-fplot(@(x) s_1(x, -1), [0, to_x])
+start_point = -10;
+% end_point = -500;
+end_point = -100;
+step = -10;
+points = start_point:step:end_point;
+N = size(points, 2);
 
-hold off
-xlabel('z_{m1A}')
-ylabel('Fe_{m1A}')
+corr = zeros(N, 1);
 
-% disp(f)
-% disp(' ')
-% disp(gof)
+%% Find the best b_E1
+parfor n = 1:N
+    warning off
+    b = points(n);
+    
+    [f, gof] = fit(data_x_1, data_y_1, 'rat03', 'Upper', [b Inf Inf Inf], 'Lower', [b -Inf -Inf -Inf]);
+    cv = coeffvalues( f );
 
-%% 2
-data_x = z_m2A;
-data_y = Fe_m2A;
-to_x = data_x(end);
-ik = -2;
+    bE1 = cv(1)/ik_1 - abs(ik_1);
+    eq = @(x, ik) eq_base(x, ik, cv, bE1);
 
-rsquared(data_x, data_y, @(x) s_1(x, ik))
+%     r2_1 = rsquared(data_x_1, data_y_1, @(x) eq(x, ik_1, cv, bE1))
+    coefs = corrcoef(eq(data_x_1, ik_1), data_y_1);
+    r2_1 = coefs(1, 2);
+    
+%     r2_2 = rsquared(data_x_2, data_y_2, @(x) eq(x, ik_2, cv, bE1));
+    coefs = corrcoef(eq(data_x_2, ik_2), data_y_2);
+    r2_2 = coefs(1, 2);
+    vpa(r2_2)
+    
+    corr(n, 1) = r2_1 * r2_2;
+    
+    if(b == start_point || b == end_point)
+        figure
 
-subplot(2, 1, 2)
-scatter(data_x, data_y)
-hold on
-fplot(@(x) s_1(x, -2), [0, to_x])
-hold off
-xlabel('z_{m2A}')
-ylabel('Fe_{m2A}')
+        subplot(2, 1, 1)
+        plot(data_x_1, data_y_1)
+        hold on
+        fplot(@(x) eq(x, ik_1), [0, to_x_1])
+        hold off
+        title(['b = ', num2str(b)])
 
-% disp(f)
-% disp(' ')
-% disp(gof)
+        subplot(2, 1, 2)
+        plot(data_x_2, data_y_2)
+        hold on
+        fplot(@(x) eq(x, ik_2), [0, to_x_2])
+        hold off
+    end
+end
+
+[v, i] = max(corr);
+i
+vpa(v)
+
+%% Final
+% 
+% subplot(2, 1, 1)
+% scatter(data_x, data_y)
+% hold on
+% fplot(@(x) s_1(x, -1), [0, to_x])
+% 
+% hold off
+% xlabel('z_{m1A}')
+% ylabel('Fe_{m1A}')
+% rsquared(data_x, data_y, @(x) s_1(x, ik))
+% 
+% subplot(2, 1, 2)
+% scatter(data_x, data_y)
+% hold on
+% fplot(@(x) s_1(x, -2), [0, to_x])
+% hold off
+% xlabel('z_{m2A}')
+% ylabel('Fe_{m2A}')
--- matlab/Approximation/rsquared.m
+++ matlab/Approximation/rsquared.m
@@ -1,13 +1,9 @@
 function [ out ] = rsquared( data_x, data_y, eq )
-
-    N = size(data_x, 2);
-    y_ = 1/N*sum(data_y);
+    y_ = mean(data_y);
     
-    top =  sum((arrayfun(@(x) eq(x), data_x) - y_).^2);
+    top =  sum((eq(data_x) - y_).^2);
     bot =  sum((data_y - y_).^2);
 
     out = top/bot;
-    out
-%     corr(
 end
 
