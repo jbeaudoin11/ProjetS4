c0cbe81466044b93ba691c953cc10e7ca3e8d071
Jimmy Beaudoin <jbeaudoin11@github.com>
2017-07-18 21:14:58 -0400

    update

--- matlab/Camera/Camera/load_image.cpp
+++ matlab/Camera/Camera/load_image.cpp
@@ -1,4 +1,6 @@
 #include <boost/shared_array.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
 #include <cstdlib>
 #include <chrono>
 #include <iostream>
@@ -7,9 +9,11 @@
 
 #include "ball_image_processing_plugin.cpp"
 
-#define BENCHMARK_IT 10000
+#define BENCHMARK_IT 1000
 
 using namespace std;
+using namespace boost;
+using namespace boost::filesystem;
 
 namespace {
 	const unsigned int IMAGE_WIDTH = 480;
@@ -19,50 +23,45 @@ namespace {
 
 int main( int argc, char **argv)
 {
-	// if(argc != 2)
-	// {
-	// 	cerr << "Erreur: Vous devez specifier seulement l'image a charger" << endl;
-	// 	return EXIT_FAILURE;
-	// }
+	// Read images
+	vector<shared_array<uint8_t>> imgs;
 
-	// Open image file
-	// ifstream image_file("imgs\\" + argv[1], ios::binary);
-	// ifstream image_file(argv[1], ios::binary);
-	ifstream image_file("imgs/image_0.rgb", ios::binary);
-	if(!image_file)
-	{
-		cerr << "Erreur: Chemin de l'image invalide" << endl;
-		return EXIT_FAILURE;
+	directory_iterator end();
+	for(auto& file : make_iterator_range(directory_iterator(path("../benchmark_imgs/")))) {
+		cout << file.path().string() << endl;
+
+		ifstream image_file(file.path().string(), ios::binary);
+		// image_file.seekg(0, image_file.beg);
+
+		// Read file
+		shared_array<uint8_t> image(new uint8_t[IMAGE_SIZE]);
+		image_file.read(reinterpret_cast<char *>(image.get()), IMAGE_SIZE);
+		imgs.push_back(image);
 	}
-
-	// Check file size
-	image_file.seekg(0, image_file.end);
-	if(image_file.tellg() != IMAGE_SIZE)
-	{
-		cerr << "Erreur: La taille de l'image specifiee est incorrecte" << endl;
-		return EXIT_FAILURE;
-	}
-	image_file.seekg(0, image_file.beg);
-
-	// Read file
-	boost::shared_array<uint8_t> image(new uint8_t[IMAGE_SIZE]);
-	image_file.read(reinterpret_cast<char *>(image.get()), IMAGE_SIZE);
-
-	BallImageProcessingPlugin plugin;
-	double x, y;
-	int i = BENCHMARK_IT;
-
-	clock_t start_time = clock();
-	while(i--) {
-		plugin.OnImage(image, IMAGE_WIDTH, IMAGE_HEIGHT, x, y);
-		// cout << "(" << to_string(x) << ", " << to_string(y) << ") "<< endl;
-	}
-	clock_t end_time = clock();
-	double duration = (end_time - start_time) / (double) CLOCKS_PER_SEC;
-
-	cout << "IT : " << to_string(BENCHMARK_IT) << endl;
-	cout << "TIME : " << to_string(duration) << endl;
-	cout << "IMG/S : " << to_string(BENCHMARK_IT/duration) << endl;
 
 	return EXIT_SUCCESS;
+
+
+	// // Benchmark
+	// BallImageProcessingPlugin plugin;
+	// double x, y;
+	// int img_index = 0;
+	// int imgs_len = imgs.size();
+	
+	// clock_t start_time = clock();
+	// int i = BENCHMARK_IT;
+	// while(i--) {
+	// 	plugin.OnImage(imgs[i], IMAGE_WIDTH, IMAGE_HEIGHT, x, y);
+	// 	img_index = img_index % imgs_len;
+
+	// 	// cout << "(" << to_string(x) << ", " << to_string(y) << ") "<< endl;
+	// }
+	// clock_t end_time = clock();
+	// double duration = (end_time - start_time) / (double) CLOCKS_PER_SEC;
+
+	// cout << "IT : " << to_string(BENCHMARK_IT) << endl;
+	// cout << "TIME : " << to_string(duration) << endl;
+	// cout << "IMG/S : " << to_string(BENCHMARK_IT/duration) << endl;
+
+	// return EXIT_SUCCESS;
 }
